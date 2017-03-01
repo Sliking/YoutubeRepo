16b72b4b915fd07769b1f6bcb8edfb90e289ade0
Miguel Pinto <miguelpinto25@hotmail.com>
2017-03-01 15:40:14 +0000

    Refactored code and added language detection API

--- java/src/main/java/com/google/api/services/samples/youtube/cmdline/data/ChannelListByRegion.java
+++ java/src/main/java/com/google/api/services/samples/youtube/cmdline/data/ChannelListByRegion.java
@@ -23,6 +23,16 @@ import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
+import com.google.common.base.Optional;
+import com.optimaize.langdetect.LanguageDetector;
+import com.optimaize.langdetect.LanguageDetectorBuilder;
+import com.optimaize.langdetect.i18n.LdLocale;
+import com.optimaize.langdetect.ngram.NgramExtractors;
+import com.optimaize.langdetect.profiles.LanguageProfile;
+import com.optimaize.langdetect.profiles.LanguageProfileReader;
+import com.optimaize.langdetect.text.CommonTextObjectFactories;
+import com.optimaize.langdetect.text.TextObject;
+import com.optimaize.langdetect.text.TextObjectFactory;
 
 public class ChannelListByRegion {
 
@@ -48,13 +58,16 @@ public class ChannelListByRegion {
     	
     	connectToDB();
     	
+    	loadKey();
+    	
     	//locationCircles();
     	
     	lisbonArea();
     	
+    	//perCountryAndDescription();
     }
     
-    public static void lisbonArea(){
+    public static void loadKey(){
     	Properties properties = new Properties();
         try {
             InputStream in = ChannelListByRegion.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
@@ -66,6 +79,15 @@ public class ChannelListByRegion {
             System.exit(1);
         }
         
+        apiKey = properties.getProperty("youtube.apikey");
+    }
+    
+    public static void testInSmallGroup(){
+    	
+    }
+    
+    public static void perCountryAndDescription(){
+
         try {
             youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
                 @Override
@@ -74,9 +96,98 @@ public class ChannelListByRegion {
             }).setApplicationName("youtube-cmdline-channellistbyregion").build();
             
             YouTube.Search.List search = youtube.search().list("snippet");
+
+            String nextToken = "";
+            List<SearchResult> searchResults = new ArrayList<SearchResult>();
             
-            apiKey = properties.getProperty("youtube.apikey");
+            //double increment = 0.4499640028797696; -- 50km
+            double increment = 0.0899928005759539; //10km
             
+            double limitW = -9.5020795;
+            double limitE = -6.1928558;
+            double limitN = 42.1438053;
+            double limitS = 36.9655293;
+            
+            double latitude = limitN;
+            double longitude = limitW;
+            
+            while(latitude >= limitS){
+            	while(longitude <= limitE){
+            		//Parameters    
+                    search.setKey(apiKey);
+                    search.setLocation("" + latitude +"," + longitude);
+                    search.setLocationRadius("10km");
+                    search.setType("video");
+                    search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                    search.setFields("items(snippet/channelId)");
+                    
+                    do{
+                    
+        	            search.setPageToken(nextToken);
+        	            SearchListResponse searchResponse = search.execute();
+        	            searchResults.removeAll(searchResponse.getItems());
+        	            searchResults.addAll(searchResponse.getItems());
+
+        	            nextToken = searchResponse.getNextPageToken();
+        	            
+                    } while(nextToken != null);
+                    
+                    longitude += increment;
+                    
+                    System.out.println("Horizontal move - coordinates (" + latitude + "," + longitude + ")");
+            	}
+            	
+            	longitude = limitW;
+            	latitude -= increment;
+            	System.out.println("================================================");
+            	System.out.println("Vertical move - coordinates (" + latitude + "," + longitude + ")");
+            	System.out.println("================================================");
+            }	
+
+            if (searchResults != null) {
+            	
+            	System.out.println("================================================");
+            	System.out.println("Inserting in local database");
+            	System.out.println("================================================");
+            	
+            	HashSet<String> hs = new HashSet<String>();
+            	
+            	for (SearchResult searchResult : searchResults) {
+                	String channelID = searchResult.getSnippet().getChannelId();
+            		if(getChannelCountry(channelID) || getChannelDescriptionLanguage(channelID)){
+            			hs.add(channelID);
+            		}
+            	}
+            	
+            	int counter = 0;
+            	for(String channelid : hs){
+            		insertIntoDB(channelid);
+            		counter++;
+            		System.out.println("Added! Total channelid: " + counter);
+            	}
+            }
+        }
+            		      
+		catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } 
+    }
+    
+    public static void lisbonArea(){
+        try {
+            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+                @Override
+                public void initialize(HttpRequest request) throws IOException {
+                }
+            }).setApplicationName("youtube-cmdline-channellistbyregion").build();
+            
+            YouTube.Search.List search = youtube.search().list("snippet");
+                        
             String nextToken = "";
             List<SearchResult> searchResults = new ArrayList<SearchResult>();
             
@@ -120,7 +231,7 @@ public class ChannelListByRegion {
             	
             	for (SearchResult searchResult : searchResults) { 
                 	String channelID = searchResult.getSnippet().getChannelId();
-            		if(getChannelCountry(channelID) && verifyIfExists(channelID)){
+            		if(getChannelCountry(channelID) && verifyIfExists(channelID) && getChannelDescriptionLanguage(channelID)){
             			hs.add(channelID);
             		}
             	}
@@ -145,17 +256,6 @@ public class ChannelListByRegion {
     }
     
     public static void locationCircles(){
-    	Properties properties = new Properties();
-        try {
-            InputStream in = ChannelListByRegion.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
-            properties.load(in);
-
-        } catch (IOException e) {
-            System.err.println("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause()
-                    + " : " + e.getMessage());
-            System.exit(1);
-        }
-        
         try {
             youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
                 @Override
@@ -164,9 +264,7 @@ public class ChannelListByRegion {
             }).setApplicationName("youtube-cmdline-channellistbyregion").build();
             
             YouTube.Search.List search = youtube.search().list("snippet");
-            
-            apiKey = properties.getProperty("youtube.apikey");
-            
+                        
             String nextToken = "";
             List<SearchResult> searchResults = new ArrayList<SearchResult>();
             
@@ -326,6 +424,44 @@ public class ChannelListByRegion {
 		}
         
         return false;
+    }
+    
+    public static boolean getChannelDescriptionLanguage(String channelID){
+    	try {
+        	
+        	ChannelListResponse channelListResponse = youtube.channels().
+                    list("snippet").setKey(apiKey).setId(channelID).execute();
+        	
+        	List<Channel> channelList = channelListResponse.getItems();
+        	        	
+        	if (channelList.isEmpty()) {
+                System.out.println("Can't find a channel with ID: " + channelID);
+                return false;
+            }        	
+        	//load all languages:
+    		List<LanguageProfile> languageProfiles;
+			languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+			//build language detector:
+    		LanguageDetector languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+    		        .withProfiles(languageProfiles)
+    		        .build();
+
+    		//create a text object factory
+    		TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText();
+
+    		//query:
+    		TextObject textObject = textObjectFactory.forText(channelList.get(0).getSnippet().getDescription());
+    		Optional<LdLocale> lang = languageDetector.detect(textObject);
+    		
+    		if(lang.get().getLanguage().equals("pt")){
+    			return true;
+    		}
+        	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
     }
     
     public static boolean getVideoCountry(String videoID){
